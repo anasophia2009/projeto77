238d5c2f4136ffd66e9817694ddd3757bbd4b7bc
anasophia2009 <101888112+anasophia2009@users.noreply.github.com>
2023-05-22 20:41:15 -0400

    Update Home.js

--- screens/Home.js
+++ screens/Home.js
@@ -26,10 +26,10 @@ export default class HomeScreen extends Component {
                         source={require('../assets/stars.gif')} 
                         style={styles.backgroundImage}> 
                 */}
-                {/* <ImageBackground 
+                 <ImageBackground 
                         source={require('../assets/stars.gif')} 
                         style={styles.backgroundImage}> 
-                */}
+                />
                 {/* <BackgroundImage
                         source={require('../assets/stars.gif')} 
                         style={styles.backgroundImage}> 
